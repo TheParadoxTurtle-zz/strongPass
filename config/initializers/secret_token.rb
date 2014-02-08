cd9f3608944453fa1628d055371432cc691f1762
Minh Tri Pham <minhtri@pham.cz>
2014-02-07 21:37:51 -0500

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strongpass::Application.config.secret_token = 'a2815cdf847da8ab629380497b045085a2fd2b4cb78391579fc65535b0c612599fd774e67bb31f309c9e46234b174fc796cf7ac0856d340d16e4cae6b8e7017a'
