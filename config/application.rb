a867ec7e66bea6a2c4cc98f205cddbd96aaefea3
Minh Tri Pham <minhtri@pham.cz>
2014-02-08 09:00:30 -0500

    herokuprep1

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module Strongpass
     config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
-    # config.assets.precompile += %w[edge.3.0.0.min.js NightDay_edge.js NightDay_edgeActions.js]
+    config.assets.precompile += %w[edge.3.0.0.min.js NightDay_edge.js NightDay_edgeActions.js]
 
   end
 end
